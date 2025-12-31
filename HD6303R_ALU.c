31c4ac3f7e3aadd2413b18bf39181a77bbbea008
bigowlrrww <bigowlrrww@gmail.com>
2025-12-31 09:48:02 -0800

    BLT Implemented

--- HD6303R_ALU.c
+++ HD6303R_ALU.c
@@ -3814,7 +3814,6 @@ void ALU_HD6303R_BLS(HD6303R_MPU * p)
 	ALU_HD6303R_SetFlag(p, HD6303R_FLAG_IMP);
 }
 
-// NOT IMPLEMENTED
 /*
 		void ALU_HD6303R_BLT(HD6303R_MPU * p)
 		Branch Test:	N ^ V = 1
@@ -3824,19 +3823,19 @@ void ALU_HD6303R_BLT(HD6303R_MPU * p)
 	uint8_t instruction = (uint8_t)MemoryRead(p, p->pc);
 	uint8_t unsigned_payload = (uint8_t)MemoryRead(p, (p->pc+1));
 	int8_t signed_payload = (int8_t)MemoryRead(p, (p->pc+1));
-	uint16_t unsigned_payload_double = uint16_From_uint8s(MemoryRead(p, (p->pc+1)), MemoryRead(p, (p->pc+2)));
-	uint16_t direct_address = (uint16_t)unsigned_payload;
 
 	switch (instruction) {
 		case 0x2D: // BLT Immediate
 			ALU_HD6303R_SetCurrentMneunomicWithPayload(p, "BLT #$%02X", unsigned_payload);
+			if (ALU_HD6303R_GetFlag(p, HD6303R_FLAG_N) ^ ALU_HD6303R_GetFlag(p, HD6303R_FLAG_V) != 0)
+				p->pc += signed_payload;
 			ALU_HD6303R_IncrementPC(p, 1);
 			break;
 		default:
 			break;
 	}
 	ALU_HD6303R_UnsetFlag(p, HD6303R_FLAG_VERIFIED);
-	ALU_HD6303R_UnsetFlag(p, HD6303R_FLAG_IMP);
+	ALU_HD6303R_SetFlag(p, HD6303R_FLAG_IMP);
 }
 
 /*
